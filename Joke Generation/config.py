eb25a57573bc31850b54b2e352adc681ebaa124c
JR Oakes <jroakes@gmail.com>
2020-07-26 07:37:45 -0400

    Update config.py

--- Joke Generation/config.py
+++ Joke Generation/config.py
@@ -4,7 +4,7 @@ from transformers import GPT2Tokenizer
 BATCH_SIZE = 16
 EPOCHS = 4
 LEARNING_RATE = 2e-5
-MAX_LEN = 64
-TRAIN_PATH = "/kaggle/input/short-jokes/shortjokes.csv"  #ADD PATH TO YOUR DATASET HERE
-MODEL_FOLDER = "/kaggle/working/trained_models"  # ADD PATH TO WHERE YOU WANT TO SAVE YOUR MODEL
+MAX_LEN = 256
+TRAIN_PATH = "data.csv"  #ADD PATH TO YOUR DATASET HERE
+MODEL_FOLDER = "trained_models"  # ADD PATH TO WHERE YOU WANT TO SAVE YOUR MODEL
 Tokenizer = GPT2Tokenizer.from_pretrained('gpt2-medium')
