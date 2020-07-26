c9514f92ddb12fae5f852c7471a6f58a987478ca
JR Oakes <jroakes@gmail.com>
2020-07-26 07:34:59 -0400

    Update config.py

--- Humorous Sentence Completion/config.py
+++ Humorous Sentence Completion/config.py
@@ -3,7 +3,7 @@ from transformers import GPT2Tokenizer
 BATCH_SIZE = 16
 EPOCHS = 4
 LEARNING_RATE = 3e-5
-MAX_LEN = 64
-TRAIN_PATH = "/content/gdrive/My Drive/shortjokes.csv"
-MODEL_FOLDER = "/content/gdrive/My Drive/Colab Notebooks/trained_models"
+MAX_LEN = 256
+TRAIN_PATH = "data.csv"
+MODEL_FOLDER = "trained_models"
 Tokenizer = GPT2Tokenizer.from_pretrained('gpt2-medium')
